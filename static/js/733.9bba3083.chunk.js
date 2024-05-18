6c5a87783523f6a7241f66f53986531375902289
Filip-Povidernyi <137682962+Filip-Povidernyi@users.noreply.github.com>
2024-05-18 14:34:49 +0000

    Deploying to gh-pages from @ Filip-Povidernyi/Camper@6047c9cf92f6889af469dc5372f9c09e03555154 🚀

---
+++ static/js/733.9bba3083.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcampers_rental=self.webpackChunkcampers_rental||[]).push([[733],{733:function(e,r,n){n.r(r);var s=n(184);r.default=function(){return(0,s.jsx)("div",{children:(0,s.jsx)("h1",{children:"Favorites"})})}}}]);
+//# sourceMappingURL=733.9bba3083.chunk.js.map
